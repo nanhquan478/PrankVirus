8722aa54f58710d1ac098ea3604066a2ffe17374
⁂ Anh Quân ⁂ <nanhquan478@gmail.com>
2025-06-30 22:03:53 +0700

    Add files via upload

---
+++ Prank Virus 3.bat
@@ -0,0 +1,33 @@
+@echo OFF
+title Virus Detected Do Not Close Window
+echo Virus Detected! Attempting to remove virus...
+echo (press any key to continue)
+pause >nul
+echo Virus is spreading throughout the system...
+echo (press any key to destroy)
+pause >nul
+echo destroying virus...
+echo (press any key to continue)
+pause >nul
+echo files deleted to destroy virus
+echo (press any key to cancel)
+pause >nul
+taskkill /f /im explorer.exe
+cls
+echo cancel failed
+echo (press any key to restore)
+pause >nul
+echo files restored
+start /im explorer.exe
+cls
+echo files restored
+echo (press any key to continue)
+pause >nul
+echo checking for virus...
+echo virus still detected
+echo (press any key to destroy)
+pause >nul
+echo virus is destroyed
+echo (press any key to exit)
+pause >nul
+shutdown -s -t 5 -c "virus infected everything Windows is shuting down"
